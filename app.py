8127d83344ff6d44b540ea4a4b695d700b16960f
Ontaelio <indraputra@yandex.ru>
2022-08-14 19:38:49 +0300

    Final

--- app.py
+++ app.py
@@ -1,18 +1,9 @@
-# основной файл приложения. здесь конфигурируется фласк, сервисы, SQLAlchemy и все остальное что требуется для приложения.
-# этот файл часто является точкой входа в приложение
-
-# Пример
-
 from flask import Flask
 from flask_restx import Api
 
 from config import Config
-# from models import Review, Book
 from setup_db import db
-# from views.books import book_ns
-# from views.reviews import review_ns
-#
-# функция создания основного объекта app
+
 from views.movies_views import movie_ns
 from views.genres_views import genre_ns
 from views.directors_views import director_ns
@@ -23,34 +14,19 @@ def create_app(config_object):
     appl.config.from_object(config_object)
     register_extensions(appl)
     return appl
-#
-#
-# функция подключения расширений (Flask-SQLAlchemy, Flask-RESTx, ...)
+
+
 def register_extensions(appl):
     db.init_app(appl)
     api = Api(appl)
     api.add_namespace(movie_ns)
     api.add_namespace(genre_ns)
     api.add_namespace(director_ns)
-    #create_data(app, db)
+    # create_data(app, db)
 
-
-#
-#
-# функция
-# def create_data(app, db):
-#     with app.app_context():
-#         db.create_all()
-#
-#         создать несколько сущностей чтобы добавить их в БД
-#
-#         with db.session.begin():
-#
-#             db.session.add_all(здесь список созданных объектов)
 
 config = Config()
 app = create_app(config)
-# register_extensions(app)
 app.debug = True
 
 if __name__ == '__main__':
